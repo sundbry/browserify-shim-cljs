e44f122f4277de9d5b275c7117e62ccdaf5b0a29
Ryan Sundberg <ryan.sundberg@gmail.com>
2016-02-29 18:47:46 -0800

    Add clojure.tools.reader as a dependency; version 0.1.0

--- project.clj
+++ project.clj
@@ -1,8 +1,11 @@
-(defproject browserify-shim "0.1.0-SNAPSHOT"
-            :description "Finds instances of (js/require 'module') and calls
-                         browserify with -r for each of them."
+(defproject browserify-shim "0.1.0"
+  :description "Finds instances of (js/require 'module') and calls
+               browserify with -r for each of them."
 
-            :license {:name "Eclipse Public License"
-                      :url "http://www.eclipse.org/legal/epl-v10.html"}
-            :eval-in-leiningen true)
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+
+  :dependencies [[org.clojure/tools.reader "1.0.0-alpha3"]]
+
+  :eval-in-leiningen true)
 
